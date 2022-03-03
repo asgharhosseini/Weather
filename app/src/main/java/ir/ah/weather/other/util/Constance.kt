c9059410e4c9c2b6df9e67e1077f5e1203d7aeb5
Hosseini <asgharhosseini.v@gmail.com>
2022-03-03 16:51:27 +0330

    create splash fragment and UserInfoManager.kt for save default state local and provide SharedPreferencesModule.kt

--- app/src/main/java/ir/ah/weather/other/util/Constance.kt
+++ app/src/main/java/ir/ah/weather/other/util/Constance.kt
@@ -4,4 +4,5 @@ object Constance {
 
 
     const val TAG = "debug"
+    const val SHARED_PREFERENCES_NAME = "userInfo"
 }
--- app/src/main/java/ir/ah/weather/ui/splash/SplashViewModel.kt
+++ app/src/main/java/ir/ah/weather/ui/splash/SplashViewModel.kt
@@ -2,12 +2,32 @@ package ir.ah.weather.ui.splash
 
 import dagger.hilt.android.lifecycle.HiltViewModel
 import ir.ah.weather.base.BaseViewModel
+import ir.ah.weather.data.local.UserInfoManager
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class SplashViewModel @Inject constructor(
-    private val mainCoroutineDispatcher: CoroutineDispatcher
+    private val mainCoroutineDispatcher: CoroutineDispatcher,
+    private val userInfoManager: UserInfoManager
 ) : BaseViewModel(mainCoroutineDispatcher) {
 
+    fun refreshSetting() = doInMain {
+        if (userInfoManager.getLocal().isNullOrEmpty()&&
+            userInfoManager.getLocal().isNullOrEmpty()
+        ) {
+            splashEventChannel.send(SplashEvent.NavigateToSetting(false))
+        }else{
+            splashEventChannel.send(SplashEvent.NavigateToCurrentWeather(true))
+        }
+    }
+
+    private val splashEventChannel = Channel<SplashEvent>()
+    val splashEvent = splashEventChannel.receiveAsFlow()
+
+    init {
+        refreshSetting()
+    }
 }
